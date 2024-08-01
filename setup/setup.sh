cd351d754b7272f47ee725512e6a0b5ea1ad5d8b
GOESTERN-0886446 <u12112@gwdu102.global.gwdg.cluster>
2024-08-01 18:55:08 +0200

    hi

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,15 +1,21 @@
-ENV_NAME="exenv"
+ENV_NAME="danuma"
+conda update -n base -c conda-forge conda -y
 conda env remove -n $ENV_NAME -y
 conda create --name $ENV_NAME python=3.10 -y
 conda activate $ENV_NAME
 
-# packages
-conda install pytorch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
-# conda install -c conda-forge tensorboard -y
+# packages also in PigDetect
+conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+conda install -c conda-forge tensorboard -y
+conda install mkl=2024.0.0 -y # needs to downgraded because of some bug in other version
+
+# other packages
 conda install -c conda-forge tqdm -y
 conda install -c conda-forge torchinfo -y
 conda install -c conda-forge matplotlib -y
 conda install anaconda::scikit-learn -y
+conda install jupyter -y
+conda install pandas -y
 
-# install custom package from setup.py
-pip install -r setup/requirements.txt
+# # install custom package from setup.py
+# pip install -r setup/requirements.txt
